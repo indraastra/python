216bdae5520f221a48084583ed8655aa4fa2dd34
Vishal Talwar <rawlatv@gmail.com>
2013-10-06 21:35:46 -0700

    updating chinesepod downloader for py3.3 and to accept feed and folder as args

--- chinesepod_downloader.py
+++ chinesepod_downloader.py
@@ -1,11 +1,17 @@
 import os
+import sys
 import datetime
 import time
-import urllib2
+import urllib.request, urllib.error, urllib.parse
 import xml.etree.ElementTree as ET
 
-feed_src = "http://chinesepod.com/[ENTER YOUR FEED URL]"
-feed = ET.fromstring(urllib2.urlopen(feed_src).read())
+if len(sys.argv) != 3:
+    print("Usage: chinesepod_downloader.py <feed_url> <output_dir>")
+    sys.exit(1)
+
+feed_src = sys.argv[1]
+feed = ET.fromstring(urllib.request.urlopen(feed_src).read())
+output_dir = sys.argv[2]
 # feed_src = "\path\to\your\feed.xml"
 # feed = ET.parse(feed_src).getroot()
 
@@ -19,7 +25,7 @@ def date_for(date_str):
 
 def download(remote_uri, local_uri):
     output = open(local_uri,"wb")
-    output.write(urllib2.urlopen(remote_uri).read())
+    output.write(urllib.request.urlopen(remote_uri).read())
     output.close()
 
 if __name__ == "__main__":
@@ -31,17 +37,19 @@ if __name__ == "__main__":
         date = podcast.find("pubDate").text
         lesson_type = uri[uri.rindex("."):]
 
-        path = "%s - %s" % (time.strftime("%Y-%m-%d", date_for(date)), folder_for(title))
+        folder_name = "%s - %s" % (time.strftime("%Y-%m-%d", date_for(date)), folder_for(title))
+        base_path = os.path.join(output_dir, folder_name)
+        file_path = os.path.join(base_path, folder_name + lesson_type)
 
-        if os.path.exists(path):
-            if os.path.exists(os.path.join(path, path + lesson_type)):
-                print("Skipping found lesson [%s%s]" % (path, lesson_type))
+        if os.path.exists(base_path):
+            if os.path.exists(file_path):
+                print(("Skipping found lesson [%s%s]" % (path, lesson_type)))
                 continue
         else:
-            os.mkdir(path)
+            os.mkdir(base_path)
 
-        print("Downloading lesson [%s%s]" % (path, lesson_type))
-        download(uri, os.path.join(path, path + lesson_type))
+        print(("Downloading lesson [%s%s]" % (folder_name, lesson_type)))
+        download(uri, file_path)
         items += 1
 
         time.sleep(.5)
